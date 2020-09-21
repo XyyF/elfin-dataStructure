3408c755b81337331cfc0458fd5a0f578298ea07
xiaoyufei <915724526@qq.com>
2020-09-21 10:19:41 +0800

    feat: linked list 单向链表

--- main/linked-list/src/index.js
+++ main/linked-list/src/index.js
@@ -12,10 +12,49 @@ function Node(element) {
 }
 
 /**
- * 链表容器
+ * 链表容器 -- 单向链表
  * @constructor
  */
 function LList() {
     const head = Symbol('head')
     this.head = new Node(head)
+    this.findNode = findNode
+    this.insert = insert
+    this.display = display
+}
+
+/**
+ * 通过节点数据查找节点
+ * @param element
+ */
+function findNode(element) {
+    let currentNode = this.head.next
+    while (currentNode && currentNode.element !== element) {
+        currentNode = currentNode.next
+    }
+    return currentNode
+}
+
+/**
+ * 链表后驱插入节点
+ * @param newElement
+ * @param oldElement 旧节点
+ */
+function insert(newElement, oldElement) {
+    const oldNode = this.findNode(oldElement)
+    const newNode = new Node(newElement)
+    // tips: 注意操作顺序
+    newNode.next = oldNode.next
+    oldNode.next = newNode
+}
+
+/**
+ * 展示链表中的节点值
+ */
+function display() {
+    let currentNode = this.head.next
+    while (currentNode) {
+        console.log('display:' + currentNode.element)
+        currentNode = currentNode.next
+    }
 }
