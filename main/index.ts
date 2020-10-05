db3a0a60cc1522856c6c4dd98df99c14b6b99d4a
xiaoyufei <915724526@qq.com>
2020-10-05 16:35:15 +0800

    feat: DSLList test

--- main/index.ts
+++ main/index.ts
@@ -4,6 +4,7 @@
 
 import SingleSide from './linked-list/single-side'
 import SingleSideCycle from './linked-list/signle-side-cycle'
+import DoubleSide from './linked-list/doule-side'
 
 export const SSLList = SingleSide
 export const ElfinSSLList = SingleSide
@@ -11,9 +12,14 @@ export const ElfinSSLList = SingleSide
 export const SSCLList = SingleSideCycle
 export const ElfinSSCLList = SingleSideCycle
 
+export const DSLList = DoubleSide
+export const ElfinDSLList = DoubleSide
+
 export default {
     SSLList: SingleSide,
     ElfinSSLList: SingleSide,
     SSCLList: SingleSideCycle,
     ElfinSSCLList: SingleSideCycle,
+    DSLList: DoubleSide,
+    ElfinDSLList: DoubleSide,
 }
---
+++ test/linked-list/doule-side.test.js
@@ -0,0 +1,157 @@
+import {DSLList} from '@/build/bundle'
+
+describe('[DSLList] 双向链表', () => {
+    describe('实例func__push', () => {
+        let llist = null
+
+        beforeEach(() => {
+            llist = new DSLList()
+        })
+
+        it('push', () => {
+            // 行为
+            llist.push(1)
+            // 链表属性 - 长度、尾节点元素、尾节点后驱指针、前驱指针
+            expect(llist.length).toBe(1)
+            let newNode = llist.head.next
+            expect(newNode.element).toBe(1)
+            expect(newNode.next).toBe(llist.tail)
+            expect(newNode.prev).toEqual(llist.head)
+            expect(llist.tail.prev).toEqual(newNode)
+            // 转化为数组
+            expect(llist.transToArray(llist)).toEqual([1])
+
+            // 行为
+            llist.push(2)
+            // 链表属性 - 长度、尾节点元素、尾节点后驱指针、前驱指针
+            expect(llist.length).toBe(2)
+            newNode = llist.head.next.next
+            expect(newNode.element).toBe(2)
+            expect(newNode.next).toBe(llist.tail)
+            expect(newNode.prev).toEqual(llist.head.next)
+            expect(llist.tail.prev).toEqual(newNode)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([1, 2])
+        })
+    })
+
+    describe('实例func__pop', () => {
+        let llist = null
+
+        beforeEach(() => {
+            llist = new DSLList()
+        })
+
+        it('pop', () => {
+            llist.push(1)
+            llist.push(2)
+
+            // 行为
+            expect(llist.pop()).toBe(2)
+            // 链表属性 - 长度、尾节点元素、尾节点后驱指针、前驱
+            expect(llist.length).toBe(1)
+            let node = llist.head.next
+            expect(node.element).toBe(1)
+            expect(node.next).toEqual(llist.tail)
+            expect(node.prev).toEqual(llist.head)
+            expect(llist.tail.prev).toEqual(node)
+            // 转化为数组
+            expect(llist.transToArray(llist)).toEqual([1])
+
+            // 行为
+            expect(llist.pop()).toBe(1)
+            // 链表属性 - 长度、无尾节点
+            expect(llist.length).toBe(0)
+            expect(llist.head.next).toEqual(llist.tail)
+            expect(llist.tail.prev).toEqual(llist.head)
+            // 转化为数组
+            expect(llist.transToArray(llist)).toEqual([])
+
+            // 行为
+            expect(llist.pop()).toBe(undefined)
+            // 链表属性 - 长度、无尾节点
+            expect(llist.length).toBe(0)
+            expect(llist.head.next).toEqual(llist.tail)
+            expect(llist.tail.prev).toEqual(llist.head)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([])
+        })
+    })
+
+    describe('实例func__unshift', () => {
+        let llist = null
+
+        beforeEach(() => {
+            llist = new DSLList()
+        })
+
+        it('unshift', () => {
+            // 行为
+            llist.unshift(1)
+            // 链表属性 - 长度、尾节点元素、尾节点后驱指针、前驱指针
+            expect(llist.length).toBe(1)
+            let newNode = llist.head.next
+            expect(newNode.element).toBe(1)
+            expect(newNode.next).toEqual(llist.tail)
+            expect(newNode.prev).toEqual(llist.head)
+            expect(llist.tail.prev).toEqual(newNode)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([1])
+
+            // 行为
+            llist.unshift(2)
+            // 链表属性 - 长度、尾节点元素、尾节点后驱指针、前驱指针
+            expect(llist.length).toBe(2)
+            newNode = llist.head.next.next
+            expect(newNode.element).toBe(1)
+            expect(newNode.next).toEqual(llist.tail)
+            expect(newNode.prev).toEqual(llist.head.next)
+            expect(llist.tail.prev).toEqual(newNode)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([2, 1])
+        })
+    })
+
+    describe('实例func__shift', () => {
+        let llist = null
+
+        beforeEach(() => {
+            llist = new DSLList()
+        })
+
+        it('shift', () => {
+            llist.unshift(1)
+            llist.unshift(2)
+
+            // 行为
+            expect(llist.shift()).toBe(2)
+            // 链表属性 - 长度、尾节点元素、尾节点后驱指针、前驱指针
+            expect(llist.length).toBe(1)
+            let node = llist.head.next
+            expect(node.element).toBe(1)
+            expect(node.next).toEqual(llist.tail)
+            expect(node.prev).toEqual(llist.head)
+            expect(llist.tail.prev).toEqual(node)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([1])
+
+            // 行为
+            expect(llist.shift()).toBe(1)
+            // 链表属性 - 长度、无尾节点
+            expect(llist.length).toBe(0)
+            expect(llist.head.next).toEqual(llist.tail)
+            expect(llist.tail.prev).toEqual(llist.head)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([])
+
+            // 行为
+            expect(llist.shift()).toBe(undefined)
+            // 链表属性 - 长度、无尾节点
+            expect(llist.length).toBe(0)
+            expect(llist.head.next).toEqual(llist.tail)
+            expect(llist.tail.prev).toEqual(llist.head)
+            // 转化为数组
+            expect(llist.transToArray()).toEqual([])
+        })
+    })
+})
